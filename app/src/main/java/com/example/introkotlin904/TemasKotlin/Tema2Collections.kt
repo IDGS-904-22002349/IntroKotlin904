ebfbc973abc91a9e6d3c0d586b1b69b9001f62b3
IDGS-904-22002349 <desarrolloparadispositivos@gmail.com>
2025-05-06 19:34:58 -0600

    if-when ultimo tema

--- app/src/main/java/com/example/introkotlin904/TemasKotlin/Tema2Collections.kt
+++ app/src/main/java/com/example/introkotlin904/TemasKotlin/Tema2Collections.kt
@@ -27,6 +27,23 @@ val coches2 = mutableMapOf("uno" to 1, "dos" to 2, "tres" to 3)
 println(coches2)
 
  */
+fun main(){
+    val readOnlyFiguras = listOf("cuadrado","triangulo","circulo")
+    println(readOnlyFiguras)
+    println("La primera figura es ${readOnlyFiguras[0]}")
+    println("el primer elemento es ${readOnlyFiguras.first()}")
+    println("numero de elementos ${readOnlyFiguras.count()} items")
+    println("circulo" in readOnlyFiguras)
+    println(readOnlyFiguras)
+    //readOnlyFiguras.add("pentagono")
+    var figura: MutableList<String> = mutableListOf("cuadrado2","triangulo2","circulo2")
+    println(figura)
+    figura.add("pentagono2")
+    println(figura)
+    figura.remove("cuadrado2")
+    println(figura)
+
+}
 
 
 
